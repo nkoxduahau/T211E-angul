957f11172c4205ec35a9d2adbacd29539db69df2
Nguyễn Văn Khoa <95401921+nkoxduahau@users.noreply.github.com>
2022-02-24 17:46:41 +0700

    angu

---
+++ src/app/student/sudent.com.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:"student",
+  //template:"<h1> Student component</h1>" // chỗ khai báo mã HTML của component
+  templateUrl:"./sudent.com.html"
+
+})
+export class StudentComponent{
+
+}
